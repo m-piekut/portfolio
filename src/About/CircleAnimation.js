c20b95af68095f1accb5a74726272f0a0ae52b20
Marek Piekut <piekut358@gmail.com>
2021-05-17 21:31:49 +0200

    1st version end

--- src/About/CircleAnimation.js
+++ src/About/CircleAnimation.js
@@ -5,7 +5,16 @@ import {ReactComponent as Scene} from './images/circle.svg'
 const CircleAnimation = () => {
     const wrapper = useRef(null)
     useEffect(()=>{
-       
+        const [elements] = wrapper.current.children;
+
+        const character = elements.getElementById('character')
+        const circleWrapper = elements.getElementById('circleWrapper')
+        gsap.set([character, circleWrapper], {autoAlpha:0, transformOrigin: "50% 50%" });
+        const tl = gsap.timeline({defaults: {ease: 'power3.inOut'}});
+
+        tl
+        .fromTo(character, {scale:0},{scale:1, autoAlpha:1 , duration:1})
+        .fromTo(circleWrapper, {scale:2},{scale:1, autoAlpha:1, duration:1}, "-=")
 
 
     },[])
