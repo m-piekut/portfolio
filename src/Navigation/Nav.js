502dff83ff9528e49bfce75ed21013cc688eb31a
Marek Piekut <piekut358@gmail.com>
2021-05-18 17:47:07 +0200

    gh pages

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -11,11 +11,13 @@ const Nav = () => {
   const dispatch = useDispatch();
   const [content, setContent] = useState(NavLanguages.PL) 
   const [showMenu, setShowMenu] = useState(true);  
+ 
   
   const [languageInfo, setLanguageInfo] = useState(localStorage.getItem('Language'))
   
+ 
   
-  
+  const navButton = document.querySelector('.navigation__button')
   useEffect(()=>{
     if(window.innerWidth >= 992){
       setShowMenu(false)
@@ -45,23 +47,26 @@ const Nav = () => {
       }
   },[value])
 
-
+    const hideMenu = ()=>{
+      setShowMenu(!showMenu)
+      showMenu?  navButton.classList.add("navigation__button--active") : navButton.classList.remove("navigation__button--active");
+    }
 
     return ( 
         // <Router>
             <div className={`navigation ${showMenu ? 'navigation--unactive': ''}`}>
         <nav className="navigation__list">
-          <NavLink className="navigation__item " exact to="/">{content.aboutMe}</NavLink>
-          <NavLink className="navigation__item " to="/projects">{content.work}</NavLink>
-          <NavLink className="navigation__item " to="/skills">{content.skills}</NavLink>
-          <NavLink className="navigation__item " to="/contact">{content.contact}</NavLink>
+          <NavLink className="navigation__item " exact to="/" onClick={hideMenu}>{content.aboutMe}  </NavLink>
+          <NavLink className="navigation__item " to="/projects" onClick={hideMenu}>{content.work}  </NavLink>
+          <NavLink className="navigation__item " to="/skills" onClick={hideMenu}>{content.skills}  </NavLink>
+          <NavLink className="navigation__item " to="/contact" onClick={hideMenu}>{content.contact}  </NavLink>
         <div className="navigation__languageButtons">
           <button className="navigation__languageButton navigation__languageButton--PL navigation__languageButton--active" onClick={()=>handleChangeLanguage('PL')}>PL</button>
           <button className="navigation__languageButton navigation__languageButton--EN" onClick={()=> handleChangeLanguage('EN')}>EN</button>
 
         </div>
         </nav>
-        <button onClick={(e)=>{e.currentTarget.classList.toggle("navigation__button--active"); setShowMenu(!showMenu)}} className="navigation__button  drop ">
+        <button  onClick={(e)=>{e.currentTarget.classList.toggle("navigation__button--active"); setShowMenu(!showMenu)}} className="navigation__button  drop ">
           <div className="navigation__bar navigation__bar--first"></div>
           <div className="navigation__bar navigation__bar--second"></div>
           <div className="navigation__bar navigation__bar--second2"></div>
